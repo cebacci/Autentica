70a96fe2ffee9e45127ecc5b9e814127eeff0c9a
cebacci <cesare.bacci@tiscali.it>
2020-12-03 17:44:54 +0100

    Add files via upload

--- python/esempio.py
+++ python/esempio.py
@@ -1,1 +1,21 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+
+url = "https://ws-a.geninfo.it/rest/api/Autenticazione"
+
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/json"
+
+data = """
+{"apiKey": "apikey00-del0-mio0-0000-progetto0000",
+          "nonce": "123456",
+          "userName": "il-mio-username",
+          "improntaPwd": "la-mia-impronta-sha256",
+          "idDispositivo": "il-mio-dispositivo"}
+"""
+
+
+resp = requests.post(url, headers=headers, data=data)
+
+print(resp.status_code)
 
