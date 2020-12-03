13c13ac2a01cb8c00353faf1ad39047314c35a02
cebacci <cesare.bacci@tiscali.it>
2020-12-03 17:44:12 +0100

    Add files via upload

--- csharp-dotnet/esempio.cs
+++ csharp-dotnet/esempio.cs
@@ -1,1 +1,25 @@
+var url = "https://ws-a.geninfo.it/rest/api/Autenticazione";
 
+var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+httpRequest.Method = "POST";
+
+httpRequest.ContentType = "application/json";
+
+var data = @"{""apiKey"": ""apikey00-del0-mio0-0000-progetto0000"",
+          ""nonce"": ""123456"",
+          ""userName"": ""il-mio-username"",
+          ""improntaPwd"": ""la-mia-impronta-sha256"",
+          ""idDispositivo"": ""il-mio-dispositivo""}";
+
+using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+{
+   streamWriter.Write(data);
+}
+
+var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+{
+   var result = streamReader.ReadToEnd();
+}
+
+Console.WriteLine(httpResponse.StatusCode);
